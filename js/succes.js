fc67d99db7f822cec547aada180fcd6aa33d8e66
Swizz540 <quentin.g@laposte.net>
2015-01-02 16:03:04 +0100

    'Succes' page enhancements

--- js/succes.js
+++ js/succes.js
@@ -1,10 +1,15 @@
-define(['init', 'libs/packery.min'], function(_, Packery) {
+define(['init', 'libs/packery.min', 'libs/magnific-popup.min'], function(_, Packery, _) {
 
 	var pckry = new Packery( '#container', {
 		// options
 		itemSelector: '.item',
 		isFitWidth: true,
 		isOriginTop: false
+	});
+
+
+	$('.popup-with-zoom-anim').magnificPopup({
+		mainClass: 'my-mfp-zoom-in'
 	});
 
 });
